273cfb7cf44eeb9bebaf4512ea8e6bf356a9b298
Laurence Lesmoras <llesmoras@proton.me>
2023-10-11 23:03:44 +0800

    Added Compile and Run cr.py

---
+++ cr.py
@@ -0,0 +1,6 @@
+# Compile and Run
+# Short script by exdarku
+from os import system
+import sys
+system('g++ ' + sys.argv[1])
+system('./a.out')
--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 
 using namespace std;
 
+void testFunction() {
+    cout << "This is a test function" << endl;
+}
+
 int main() {
     cout << "Hello world!" << endl;
+    testFunction();
     return 0;
 }
+
