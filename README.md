32b0745ffae8707d296ca7541c3ba78c89b02c44
Laurence Lesmoras <llesmoras@proton.me>
2023-10-15 01:04:41 +0800

    Base app completed

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-## MAAM CHERRY WE LOVE YOU MWAH MWAH
+## CS101L - Summative Assessment Task
+> Ma'am Cherry, love ka daw ni Jovan Morden
 
-Maam cherry bahog
+
+### Codebase
+
+####TO DOs
+- [x] Make barebones application working
+- [x] User can set a nickname
+- [x] Timer for next task
+- [ ] Play sound when task complete
+- [ ] Send motivational message after completing task
+    - Pepper's task
+- [ ] Save data
+- [ ] See the history of the user's usage
+
+#### Changelog 
+**Commit** `PlaceholderText`
+- Base app completed
+
+
+### Documentation
+####TO DOs
+- [ ] Start the Documentation
+#### Changelog 
+Nothing here yet!
--- main.cpp
+++ main.cpp
@@ -7,7 +7,18 @@
 
 
 
+
 std::string appName = "Study Buddy"; // App Name
+
+void cpsleep(int time) {
+    #ifdef WINDOWS
+    sleep(time);
+    #else
+    // Assume POSIX
+    sleep(time);
+    #endif
+};
+
 
 void clear()
 {
@@ -37,11 +48,15 @@ public:
 User loggedInUser = User("None");
 
 class App { // Main App Class
+private:
+    std::string taskName;
+    int taskDuration;
+    tsl::ordered_map<std::string, int> todo;
 public:
     void header() {
     clear();
     std::cout << "=========================================================================" << std::endl;
-    std::cout << rang::fg::cyan << appName << rang::fg::gray << std::endl;
+    std::cout << rang::fg::cyan << rang::style::bold << appName << rang::fg::reset << rang::style::reset << std::endl;
     std::cout << "=========================================================================" << std::endl;
     };
 
@@ -56,23 +71,20 @@ public:
 
     void appMenu() {
         int currentTask = 1;
-        std::string taskName;
-        int taskDuration;
-        tsl::ordered_map<std::string, int> todo;
         while(true){
             header();
-            std::cout << "Welcome to " << appName << ", " << loggedInUser.getUsername() << "!" << std::endl;
+            std::cout << "Welcome to " << appName << ", " << rang::style::bold << rang::fg::green << loggedInUser.getUsername() << rang::style::reset << rang::fg::reset <<"!" << std::endl;
             std::cout << "Enter the tasks you want to do for this session" << std::endl;
             std::cout << "Don't forget to type 'done' in the task name to proceed!" << std::endl;
             std::cout << "=========================================================================" << std::endl;
-            std::cout << "Current Task List: " << std::endl;
+            std::cout << "Current Task List: \n" << std::endl;
             int count = 0;
             for ( const auto &p : todo){
                 count = count + 1;
                 std::cout << "[" << count << "] " << p.first << "   -   " << p.second << " minutes"  << std::endl;
-            } 
-            std::cout << "=========================================================================" << std::endl;
-            std::cout << "[" << currentTask << "] " << "Task name: "; 
+            };
+
+            std::cout << "\n[" << currentTask << "] " << "Task name: "; 
             std::getline(std::cin, taskName); // We use getline para mu take ug input with space.
             if (taskName == "done") {
                 break;
@@ -85,6 +97,34 @@ public:
             };
         };
     };
+
+    void appCountdownScreen() {
+        header();
+        int pause = 0;
+        int count = 0;
+        std::cout << "Yipee! All tasks are now in queue! type 'go' and press enter to start with your tasks!\n" << std::endl;
+        for(const auto &p : todo){
+            count = count + 1;
+            std::cout << "[" << count << "] " << p.first << "   -   " << p.second << " minutes"  << std::endl;
+        };
+        std::cout << "=========================================================================" << std::endl;
+        std::cin >> pause;
+        // Start the queue
+        int loopcount = 0;
+        for(const auto &p : todo){
+            loopcount = loopcount + 1;
+            int currentTaskDuration = p.second * 60;
+            while(true){
+                header();
+                std::cout << "[" << loopcount << "] " << p.first << "   -   " << currentTaskDuration << " seconds \n";
+                cpsleep(1);
+                currentTaskDuration = currentTaskDuration - 1;
+                if(currentTaskDuration == 0) {
+                    break;
+                };
+            };
+        };
+    };
 };
 App AppInstance; // Initializes the App class
 
@@ -93,6 +133,7 @@ App AppInstance; // Initializes the App class
 int main() {
     AppInstance.registerScreen();
     AppInstance.appMenu();
+    AppInstance.appCountdownScreen();
     return 0;
 };
 
