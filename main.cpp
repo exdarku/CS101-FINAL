ccac69503fbaaf7c6480c85b4606eac9d1f89238
Laurence <exdarku@gmail.com>
2023-10-29 17:30:44 +0800

    Fixed master branch mishap

--- main.cpp
+++ main.cpp
@@ -4,24 +4,31 @@
 #include <map>
 #include "include/tsl/ordered_map.h" // For ordered list
 #include <cstdlib>
+#ifdef LINUX
+#include <unistd.h>
+#endif
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 
-const std::string appName = "Study Buddy"; // App Name
 
 
-void cpsleep(int time) { 
-    #ifdef WINDOWS
-    sleep(time);
-    #else
-    // Assume POSIX
-    sleep(time);
-    #endif
+std::string appName = "Study Buddy"; // App Name
+
+void cpsleep(int time) {
+#ifdef LINUX
+    usleep(time * 1000);   // usleep takes sleep time in us (1 millionth of a second)
+#endif
+#ifdef _WIN32
+    Sleep(time * 1000);
+#endif
 };
 
 
 void clear()
 {
-#ifdef WINDOWS
+#ifdef _WIN32
     std::system("cls");
 #else
     // Assume POSIX
@@ -29,7 +36,7 @@ void clear()
 #endif
 }
 
-class User { // User object | Setter/Getter
+class User { // User object
 private:
     std::string username;
 public:
@@ -44,31 +51,28 @@ public:
     };
 };
 User loggedInUser = User("None");
-
-
-
 class App { // Main App Class
 private:
     std::string taskName;
     int taskDuration;
     tsl::ordered_map<std::string, int> todo;
 public:
-    void header() { 
-        clear();
-        std::cout << "=========================================================================" << std::endl;
-        std::cout << rang::fg::cyan << rang::style::bold << appName << rang::fg::reset << rang::style::reset << std::endl;
-        std::cout << "=========================================================================" << std::endl;
+    void header() {
+    clear();
+    std::cout << "=========================================================================" << std::endl;
+    std::cout << rang::fg::cyan << rang::style::bold << appName << rang::fg::reset << rang::style::reset << std::endl;
+    std::cout << "=========================================================================" << std::endl;
     };
-
     void registerScreen() {
         header();
+        std::string pause;
         std::string username;
         std::cout << "Howdy there! Register with your name or nickname to continue!\nNickname: ";
         std::getline(std::cin, username);
         loggedInUser.setUsername(username);
     }
-
     void appMenu() {
+        int currentTask = 1;
         while(true){
             header();
             std::cout << "Welcome to " << appName << ", " << rang::style::bold << rang::fg::green << loggedInUser.getUsername() << rang::style::reset << rang::fg::reset <<"!" << std::endl;
@@ -79,10 +83,9 @@ public:
             int count = 0;
             for ( const auto &p : todo){
                 count = count + 1;
-                std::cout << "[" << count << "] " << p.first << "   -   " << p.second << " minutes"  << std::endl;
+                std::cout << "[" << count + 1 << "] " << p.first << "   -   " << p.second << " minutes"  << std::endl;
             };
-
-            std::cout << "\n[" << count + 1 << "] " << "Task name: "; 
+            std::cout << "\n[" << currentTask << "] " << "Task name: "; 
             std::getline(std::cin, taskName); // We use getline para mu take ug input with space.
             if (taskName == "done") {
                 break;
@@ -91,10 +94,10 @@ public:
                 std::cin >> taskDuration; 
                 std::cin.ignore();
                 todo[taskName] = taskDuration;
-            };  
+                currentTask = currentTask + 1;
+            };
         };
     };
-
     void appCountdownScreen() {
         header();
         int pause = 0;
@@ -124,11 +127,9 @@ public:
     };
 };
 App AppInstance; // Initializes the App class
-
 int main() {
     AppInstance.registerScreen();
     AppInstance.appMenu();
     AppInstance.appCountdownScreen();
     return 0;
 };
-
